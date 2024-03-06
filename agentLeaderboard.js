b6af10f0e939c9d95932c998d321ea8b9a377a02
Waynelle Ize-Iyamu <wizeiyam@uci.edugit config --global user.name waynelle ize-iyamu>
2024-03-06 15:03:36 -0800

    rename js A leaderboard

---
+++ agentLeaderboard.js
@@ -0,0 +1,36 @@
+//SOME FUNCTIONALITY TO AGENT LEADERBOARD
+
+
+//sort metric drop down alphabetically 
+$(function() {
+    function sortAlphabetically() {
+        var select = $('select');
+        select.html(select.find('option').sort(function(x, y) {
+            return $(x).text().localeCompare($(y).text());
+        }));
+    }
+    //call upon loading
+    sortAlphabetically();
+});
+
+
+//
+function clickedWeekly(){
+    document.getElementById("weekly").style.backgroundColor = "pink";
+    setTimeout(function() {
+        document.getElementById("weekly").style.backgroundColor = "#37475A";
+      }, 700); // 2000 milliseconds = 2 seconds
+    
+    //TODO change metric to look based on weekly data
+  };
+
+
+  function clickedAllTime(){
+    document.getElementById("allTime").style.backgroundColor = "green";
+    setTimeout(function() {
+        document.getElementById("allTime").style.backgroundColor = "#37475A";
+      }, 700); // 2000 milliseconds = 2 seconds
+
+    //TODO change metric to look based on all the data we have so far
+  };
+
