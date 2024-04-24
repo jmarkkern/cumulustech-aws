5ff31c9713d6de9cfea5ae005df7da7fb5e3e767
emboo67 <125615115+emboo67@users.noreply.github.com>
2024-04-24 12:15:53 -0700

    created PieChart template

--- aws-project-supervisor/src/components/Trophy.jsx
+++ aws-project-supervisor/src/components/Trophy.jsx
@@ -1,9 +1,22 @@
 import React from "react"
+import PieChart from "./PieChart";
 
 
 function Trophy(){
     return(
-       <h1/>
+        <>
+        <h1>Trophy Overview</h1>
+        <div className= "trophy-container">
+
+            <div  className= "t-contain-item1">
+                <h2>Tophy Overview</h2>
+                <PieChart />
+             </div>
+
+        </div>
+
+       
+        </>
     );
 }
 export default Trophy;
