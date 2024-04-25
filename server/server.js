d3b1e6df4500e1178b744c025e88fc48fbddfd74
Kevin <107275055+KevinJli23@users.noreply.github.com>
2024-04-24 22:56:49 -0700

    - commit without working code

--- server/server.js
+++ server/server.js
@@ -63,4 +63,6 @@ app.get("/api/AverageNonTalkTime", (req, res) => {
     });
 });
 
+const port = 4000 // Use environment variable for port or default to 4000
 app.listen(4000, () => { console.log("server started in port 4000") })
+
