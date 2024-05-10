f2def753d9a17b93a9e075ce28c6920b45a8caa4
Waynelle Ize <waynelleize@Waynelles-MacBook-Pro.local>
2024-05-09 20:14:08 -0700

    agent challenges and motivation, still need to be connected to data

---
+++ aws-project-agent/src/components/agentChallenge.jsx
@@ -0,0 +1,28 @@
+//from the server collect challenge info in the same manner as supervisor info
+
+// import React from "react"
+// import Collapse from 'react-bootstrap/Collapse';
+
+// function agentChallenge(props){
+//     const [open, setOpen] = useState(false);
+
+//     return(
+//         <div className="agentChallenges">
+//             <h1>Challenges</h1>
+//             <div className="listChallenges">
+//                 <p>{props.name}</p>
+//                 {/* progress bar */}
+//                 <button onClick={() => setOpen(!open)} >make this an arrow</button>
+//             </div>
+//             <Collapse in={open}>
+//                 <div className="extraDetails">
+//                     <strong><p>Description: {props.desc}</p></strong>
+//                     <strong><p>How To Win: {props.howTo}</p></strong>
+//                 </div>
+//             </Collapse>
+            
+//         </div>
+//     );
+// }
+// export default agentChallenge;
+//need to check, but will do after I can pull
