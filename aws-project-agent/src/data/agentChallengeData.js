ab5ae6455b35d1885ef3164df76b3cb19fe78d15
Waynelle Ize <waynelleize@Waynelles-MacBook-Pro.local>
2024-05-09 21:26:51 -0700

    agentFakeData

---
+++ aws-project-agent/src/data/agentChallengeData.js
@@ -0,0 +1,21 @@
+const challengDetail = [
+    {
+        id: 1,
+        name: "Best of the Best",
+        descrip: "We want to see you shine! This is just for fun, but who has it in them!",
+        howToWin: "Have a call of less than _______. OMG how are we going to measure how they write this",
+        points: "5",
+        date: "May 9, 2024",
+        trophy: "Customer Service Hero"
+    },
+     {
+        id: 2,
+        name: "No time wasted",
+        descrip: "Be the best",
+        howToWin: "Have a call of less than _______. OMG how are we going to measure how they write this",
+        points: "5",
+        date: "May 9, 2024",
+        trophy: "Customer Service Hero"
+    }
+]
+export default challengDetail;
