b12cd63c65d349cdc4aa1ca6b204855cc5130415
namit-s <mr.namit@gmail.com>
2024-05-12 21:30:01 -0700

    added code from avg active time.js and edited it so it displays all ranks

--- aws-project-supervisor/src/components/Leaderboard.jsx
+++ aws-project-supervisor/src/components/Leaderboard.jsx
@@ -1,8 +1,16 @@
 import React, { useState, useEffect } from "react";
+import rank1 from "../ranks/rank1.png"; 
+import rank2 from "../ranks/rank2.png";
+import rank3 from "../ranks/rank3.png";
+import rank4 from "../ranks/rank4.png";
+import rank5 from "../ranks/rank5.png";
+import '../global.css';
+
 
 
 function Leaderboard() {
 
+//current unique code for leaderboard.jsx
   const [theTeam, getTeam] = useState("");
   const [theMetric, getMetric] = useState("");
   const [theDuration, getDuration] = useState("");
@@ -50,11 +58,60 @@ function Leaderboard() {
   };
 
 
+//Code taken from average_active_time.js
+    const [names, setNames] = useState([]);
+    const [visibleRows, setVisibleRows] = useState(0);
+
+    useEffect(() => {
+        fetch("http://localhost:4000/api/AverageActiveTime")
+            .then(response => response.json())
+            .then(data => {
+                setNames(data.AverageActiveTime)
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
+    const convertTimeToSeconds = (time) => {
+        const [hours, minutes, seconds] = time.split(':').map(Number);
+        return hours * 3600 + minutes * 60 + seconds;
+    };
+
+    const sortNames = () => {
+        const sortedNames = [...names].sort((a, b) => {
+            const timeA = convertTimeToSeconds(a.time);
+            const timeB = convertTimeToSeconds(b.time);
+            return timeB - timeA; //Largest to Smallest
+        });
+        setNames(sortedNames);
+    };
+
+    const incrementVisibleRows = () => {
+        setVisibleRows(prevVisibleRows => prevVisibleRows + 1);
+    };
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            incrementVisibleRows();
+        }, 200);
+
+        if (visibleRows === 5) {
+            clearInterval(timer);
+        }
+        sortNames()
+        return () => clearInterval(timer);
+    }, [visibleRows]);
+
+    const rankBadges = [rank1, rank2, rank3, rank4, rank5];
+
+
 
 
 
 
   return (
+  
     <div className="grid-item1">
     <div className="flexMain">
         <h1>Leaderboard</h1>
@@ -78,9 +135,40 @@ function Leaderboard() {
 
     <button class="updateButton" id="leaderboardbutton" >All Time</button>
     <button class="updateButton" id="leaderboardbutton1" >This week</button>
-</div>
+    </div>
 
 
+
+
+    {/* Code taken from average_active_time.js */}
+    <div className="containerLeaderboard1">
+            <div className="item title">
+                <h1>Average Active Time</h1>
+            </div>
+            <div className="item table">
+                <table className="table3" style={{
+                    paddingLeft: '20px', paddingRight: "20px", width: '100%', borderSpacing: '0px 20px'
+                }}>
+                    <tbody>
+                        <tr>
+                            <th style={{ paddingLeft: '15px', textAlign: 'left' }}>Rank</th>
+                            <th style={{ width: '33%' }}>Agent</th>
+                            <th style={{ width: '33%' }}>Times</th>
+                        </tr>
+                        {/* Edited the code here so all ranks can be shown instead of just top 5 and also removed the css reference to "fade-in" from global.css */}
+                        {names.map((item, index) => (
+                            <tr key={index} >
+                                <td style={{ paddingLeft: '15px', textAlign: 'left', borderTopLeftRadius: '10px', borderBottomLeftRadius: '10px' }}><img src={rankBadges[index]} className='rank-badge'></img></td>
+                                <td style={{ width: '33%' }}>{item.name}</td>
+                                <td style={{ width: '33%', borderTopRightRadius: '10px', borderBottomRightRadius: '10px' }}>{item.time}</td>
+                            </tr>
+                        ))}
+
+                    </tbody>
+
+                </table>
+            </div>
+        </div >
 
 </div>
   );
